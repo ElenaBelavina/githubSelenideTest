80d47e5652168409e95f3e2b5ce4927d5f33c52a
ElenaBelavina <154471636+ElenaBelavina@users.noreply.github.com>
2024-01-12 18:31:53 +0300

    .

--- src/test/java/GithubSelenide.java
+++ src/test/java/GithubSelenide.java
@@ -8,20 +8,11 @@ import static com.codeborne.selenide.Selenide.*;
 public class GithubSelenide {
     @Test
     void softAssertionsTest(){
-        // открыть главную страницу
         open("https://github.com/selenide/selenide");
-
-        //перейти в раздел wiki
         $("#wiki-tab").click();
-
-        // Проверить, что в Pages есть SoftAssertions
         $("#wiki-pages-filter").setValue("SoftAssertions");
         $("#wiki-pages-box").$$("ul li").shouldHave(itemWithText("SoftAssertions"));
-
-        // Перейти на страницу Soft Assertions
         $("#wiki-pages-box").$(byText("SoftAssertions")).click();
-
-        // Проверить, что в тексте есть пример кода для JUnit5
         $(".markdown-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
                 "class Tests {\n" +
                 "  @Test\n" +
@@ -32,8 +23,6 @@ public class GithubSelenide {
                 "    $(\"#first\").should(visible).click();\n" +
                 "    $(\"#second\").should(visible).click();\n" +
                 "  }\n" +
-                "}"));
-
-        //sleep(6000);
+                "}"));        
     }
 }
